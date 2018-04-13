88b77ec13c28e61a68409a812c1dc2f9d6a47783
Tom Raithel <t.raithel@googlemail.com>
2018-04-13 23:12:27 +0200

    Add show instance for grid

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,22 @@
 module Main where
 
 import Control.Monad (forever)
+import qualified Data.List as L
+import qualified Data.List.Split as S
 import qualified Data.Map as M
 
 data Coord = Coord Int Int deriving(Ord, Eq, Show)
-data Cell = Dead | Alive deriving(Ord, Eq, Show)
-data Grid = Grid (M.Map Coord Cell) deriving(Ord, Eq, Show)
+data Cell = Dead | Alive deriving(Ord, Eq)
+data Grid = Grid Int (M.Map Coord Cell) deriving(Ord, Eq)
+
+instance Show Cell where
+  show Dead = " □ "
+  show _ = " ■ "
+
+instance Show Grid where
+  show (Grid s m) = L.intercalate "\n" $ map renderLine ls where
+    ls = S.chunksOf s (M.elems m)
+    renderLine l = L.concat $ L.map show l
 
 nextTick :: Grid -> Grid
 nextTick = id
@@ -13,14 +24,14 @@ nextTick = id
 runGame :: Grid -> IO ()
 runGame grid = forever $ do
   putStrLn $ show grid
-  putStrLn "Press enter to proceed"
+  putStrLn "\n> Press enter to proceed"
   _ <- getLine
   return (nextTick grid) >>= runGame
 
+makeGrid :: Int -> Grid
+makeGrid s = Grid
+  s
+  (M.fromList [ (Coord x y, Dead) | x <- [0 .. (s - 1)], y <- [0 .. (s - 1)] ])
+
 main :: IO ()
-main = runGame $ Grid (M.fromList [
-    (Coord 0 0, Dead),
-    (Coord 0 1, Dead),
-    (Coord 1 0, Dead),
-    (Coord 1 1, Dead)
-  ]);
+main = runGame $ (makeGrid 10)
